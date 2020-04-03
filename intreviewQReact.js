f7d33895a8e26da957d0c229607ace4faf41bd9a
sailajasmart <sailajasmart@gmail.com>
2020-04-03 22:20:52 +0530

    Create intreviewQReact.js

---
+++ intreviewQReact.js
@@ -0,0 +1,34 @@
+/*
+How to pass value from parent to child in react componets?
+>Define a callback in parent which takes the data I need in as a parameter.
+>Pass that callback as a prop to the child.
+>Call the callback using this.props.[callback] in the child (insert your own name where it says [callback] of course), 
+and pass in the data as the argument.
+
+
+How to share data between two component?
+
+Life Cyle methods in React?
+mounting
+-----------
+>constructor()
+>static getDerivedStateFromProps()
+>render()
+>componentDidMount()
+
+updating
+----------
+static getDerivedStateFromProps()
+shouldComponentUpdate()
+render()
+getSnapshotBeforeUpdate()
+componentDidUpdate()
+
+unmounting
+componentWillUnmount()
+
+
+What is virtual Dom in react
+https://reactjs.org/docs/faq-internals.html
+
+*/
