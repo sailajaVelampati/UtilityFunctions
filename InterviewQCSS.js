91876fa8fc54ac5a9fe685bd075f34fe13a81395
sailajasmart <sailajasmart@gmail.com>
2020-04-03 23:03:10 +0530

    Update InterviewQCSS.js

--- InterviewQCSS.js
+++ InterviewQCSS.js
@@ -1,8 +1,26 @@
 /*
 flex
 display properties
+block:takes full width
+inline:takes as much required.
+[make horizontal lis make li:display inline]
+
 box model
+margin:
+padding:
+border:
+
+
 positioning elements
+static:always positioned according to the normal flow of the page
+relative: positioned relative to its normal position
+fixed:it always stays in the same place even if the page is scrolled. The top, right, bottom, and left properties are used to position the
+element.
+absolute:is positioned relative to the nearest positioned ancestor (instead of positioned relative to the viewport, like fixed).
+However; if an absolute positioned element has no positioned ancestors, it uses the document body, and moves along with page scrolling.
+sticky:is positioned based on the user's scroll position.
+
+
 media quires
 sudo classes
 
