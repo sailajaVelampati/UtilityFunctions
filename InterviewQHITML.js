3e539cc00ed9eb275adc52cc125054a3555dad65
sailajasmart <sailajasmart@gmail.com>
2020-04-03 22:53:02 +0530

    Create InterviewQHITML.js

---
+++ InterviewQHITML.js
@@ -0,0 +1,21 @@
+Semantic structing?
+A semantic element clearly describes its meaning to both the browser and the developer.
+<article>,<aside>,<details>,<figcaption>,<figure>,<footer>,<header>,<main>,<mark>,<nav>,<section>,<summary>,<time>
+
+accesablity
+
+geolocations
+
+localstorage
+
+canvas
+
+Latest tags: 
+<mark> highlights the text with background color.
+<picture>
+The <embed> tag defines a container for an external application or interactive content 
+The <track> tag specifies text tracks for media elements (<audio> and <video>).
+This element is used to specify subtitles, caption files or other files containing text, that should be visible when the media is playing.
+
+In an audio if the one src is not working how to fix it?
+html 5 have <source> we can pass multiple sources to the audio, video, picture tags.
