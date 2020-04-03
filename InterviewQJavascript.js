f700f1826d0093da10c7a780fcb500d6bb13a276
sailajasmart <sailajasmart@gmail.com>
2020-04-03 22:35:44 +0530

    Update InterviewQJavascript.js

--- InterviewQJavascript.js
+++ InterviewQJavascript.js
@@ -1,7 +1,17 @@
 /*
 data types
 first class object in javascript?
+
 oops in javascript?
+Class
+Object
+Inheritance
+Encapsulation – The process of wrapping property and function within a single unit is known as encapsulation.
+
 bind, call, apply
+
+ways to create an object
+{}, new Object, Object.create()
+
 
 */
