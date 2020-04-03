6af9c09e6dedef060da4e438a7822e0b20c543ee
sailajasmart <sailajasmart@gmail.com>
2020-04-03 22:41:39 +0530

    Update InterviewQJavascript.js

--- InterviewQJavascript.js
+++ InterviewQJavascript.js
@@ -1,6 +1,15 @@
 /*
 data types
+
 first class object in javascript?
+In computer science, a programming language is said to support first-class functions (or function literal) if it treats functions as 
+first-class objects. Specifically, this means that the language supports constructing new functions during the execution of a program, 
+storing them in data structures, passing them as arguments to other functions, and returning them as the values of other functions.
+>A function is an instance of the Object type
+>A function can have properties and has a link back to its constructor method
+>You can store the function in a variable
+>You can pass the function as a parameter to another function
+>You can return the function from a function
 
 oops in javascript?
 Class
@@ -8,7 +17,8 @@ Object
 Inheritance
 Encapsulation – The process of wrapping property and function within a single unit is known as encapsulation.
 
-bind, call, apply
+bind, call, apply?
+call()
 
 ways to create an object
 {}, new Object, Object.create()
