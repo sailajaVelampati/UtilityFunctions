2ddf792ac3a33f7154ad366de53d7904c363274c
sailajasmart <sailajasmart@gmail.com>
2020-04-04 09:34:25 +0530

    Update InterviewQEs.js

--- InterviewQEs.js
+++ InterviewQEs.js
@@ -1,5 +1,25 @@
 /*
 latest in es6
+https://github.com/lukehoban/es6features
+arrows
+classes
+enhanced object literals
+template strings
+destructuring
+default + rest + spread
+let + const
+iterators + for..of
+generators
+unicode
+modules
+module loaders
+map + set + weakmap + weakset
+proxies
+symbols
+subclassable built-ins
+promises,math + number + string + array + object APIs,binary and octal literals,reflect api,tail calls
+
+
 arrow function
 let vs var
 
