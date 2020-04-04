b7df042375a6e97f1398bfde2d2a5865cb6e6914
sailajasmart <sailajasmart@gmail.com>
2020-04-04 09:09:45 +0530

    Update InterviewQCSS.js

--- InterviewQCSS.js
+++ InterviewQCSS.js
@@ -45,4 +45,27 @@ Also, with display: inline-block, the top and bottom margins/paddings are respec
 Compared to display: block, the major difference is that display: inline-block does not add a line-break after the element,
 so the element can sit next to other elements.
 
+How to achienve responsivness in website?
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+A <meta> viewport element gives the browser instructions on how to control the page's dimensions and scaling.
+The width=device-width part sets the width of the page to follow the screen-width of the device 
+(which will vary depending on the device).
+The initial-scale=1.0 part sets the initial zoom level when the page is first loaded by the browser.
+Users are used to scroll websites vertically on both desktop and mobile devices - but not horizontally!
+So, if the user is forced to scroll horizontally, or zoom out, to see the whole web page it results in a poor user experience.
+Some additional rules to follow:
+1. Do NOT use large fixed width elements - For example, if an image is displayed at a width wider than the viewport it can cause the 
+viewport to scroll horizontally. Remember to adjust this content to fit within the width of the viewport.
+2. Do NOT let the content rely on a particular viewport width to render well - Since screen dimensions and width in CSS pixels vary 
+widely between devices, content should not rely on a particular viewport width to render well.
+3. Use CSS media queries to apply different styling for small and large screens - Setting large absolute CSS widths for page elements 
+will cause the element to be too wide for the viewport on a smaller device. Instead, consider using relative width values, such as width: 100%. Also, be careful of using large absolute positioning values. It may cause the element to fall outside the viewport on small devices.
+4.mobile first approach?
+
+How to achieve responsiveness with bootstrap?
+set the viewport
+by default container add 15px padding to left and right.
+container-sm|md|lg|xl default sizes in boot strap 
+container can be fixed width or fluid [for fixed width we can see screen adjusting css, in fluid look is semless ].
+
 */
