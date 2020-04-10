5834ed5ddf622bad004dbe792f083e9ee785837b
sailajasmart <sailajasmart@gmail.com>
2020-04-10 13:09:25 +0530

    web workers

--- InterviewQHITML.js
+++ InterviewQHITML.js
@@ -16,7 +16,8 @@ Latest tags:
 <picture>
 The <embed> tag defines a container for an external application or interactive content 
 The <track> tag specifies text tracks for media elements (<audio> and <video>).
-This element is used to specify subtitles, caption files or other files containing text, that should be visible when the media is playing.
+This element is used to specify subtitles, caption files or other files containing text, 
+that should be visible when the media is playing.
 
 In an audio if the one src is not working how to fix it?
 html 5 have <source> we can pass multiple sources to the audio, video, picture tags.
@@ -25,4 +26,18 @@ html AIPs like geolocations, drag ans drop, web storage,
 
 What is iframe ?
 An inline frame is used to embed another document within the current HTML document.
+
+What is a Web Worker?
+When executing scripts in an HTML page, the page becomes unresponsive until the script is finished.
+A web worker is a JavaScript that runs in the background, independently of other scripts, without affecting the performance of the page.
+You can continue to do whatever you want: clicking, selecting things, etc., while the web worker runs in the background.
+postMessage() - posts message back to the browser.
+onmessage(event) - this is the vent listener event has data of web workers.
+terminate() - will terminate the event listener.
+if the webworker variavle is set to undefined it can be reused.
+Since web workers are in external files, they do not have access to the following JavaScript objects:
+>The window object
+>The document object
+>The parent object
+
 */
